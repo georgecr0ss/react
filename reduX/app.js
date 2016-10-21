3bc6325bd7054a9e17b6af0ded109cfb5eddb6bd
Georgi Krustev <georgi.krustev@trader.int>
2016-10-21 11:17:33 +0300

    completed 14th lesson

--- reduX/app.js
+++ reduX/app.js
@@ -24,6 +24,19 @@ const todo = (state, action) => {
 
   }
 }
+
+const visibilityFilter = (
+  state = 'SHOW_ALL',
+  action
+) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter
+    default:
+      return state
+  }
+}
+
 const todos = (state = [], action) => {
   switch (action.type) {
     case "ADD_TODO":
@@ -41,19 +54,29 @@ const todos = (state = [], action) => {
   }
 }
 
-const store = createStore(todos)
-
-const visibilityFilter = (
-  state = 'SHOW_ALL',
-  action
-) => {
-  switch (action.type) {
-    case 'SET_VISIBILITY_FILTER':
-      return action.filter
-    default:
-      return state
+const todoApp = (state = {}, action) => {
+  console.warn( todos(
+    state.todos,
+    action
+  ));
+  console.warn(visibilityFilter(
+    state.visibilityFilter,
+    action
+  ));
+  return {
+    todos: todos(
+      state.todos,
+      action
+    ),
+    visibilityFilter: visibilityFilter(
+      state.visibilityFilter,
+      action
+    )
   }
 }
+
+const store = createStore(todoApp)
+
 
 console.log("Current state:");
 console.log(store.getState());
@@ -74,6 +97,24 @@ store.dispatch({
   type:"ADD_TODO",
   id: 1,
   text: "Go shoping"
+})
+console.log("Current state:");
+console.log(store.getState());
+console.log("---------------");
+
+console.log("Dispatching TOGGLE_TODO");
+store.dispatch({
+  type:"TOGGLE_TODO",
+  id: 0
+})
+console.log("Current state:");
+console.log(store.getState());
+console.log("---------------");
+
+console.log("Dispatching SET_VISIBILITY_FILTER");
+store.dispatch({
+  type:"SET_VISIBILITY_FILTER",
+  filter: 'SHOW_COMPLETED'
 })
 console.log("Current state:");
 console.log(store.getState());
