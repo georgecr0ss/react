d35fa9ec8ee66a0532aebf55ff0dac90e5bd128f
Georgi Krustev <georgi.krustev@trader.int>
2016-10-24 18:00:10 +0300

    xcz

--- fluX/js/stores/app-store.js
+++ fluX/js/stores/app-store.js
@@ -52,3 +52,48 @@ const _cartTotals = ( qty = 0, total = 0 ) => {
 
   retur { qty, total }
 }
+
+cosnt AppStore = Object.assign(EventEmmiter.prototype, {
+  emitChange() {
+    this.emit( CHANGE_EVENT )
+  },
+  addChangeListener( callback ) {
+    this.on( CHANGE_EVENT, callback )
+  },
+  removeChangeListener( callback ) {
+    this.removeListener( CHANGE_EVENT, callback )
+  },
+  getCart() {
+    return _cartItems
+  },
+  getCatalog() {
+    return _catalog.map( item => {
+      return Object.assign({}, _cartItems.find( cItem => cItem.id === item.id))
+    })
+  },
+  getCartTotals() {
+    return _cartTotals()
+  },
+  dispatcherIndex: register( function( action ) {
+    // body...
+    switch (action.actionType) {
+      case AppConstants.ADD_ITEM:
+          _addItem( action.item )
+        break;
+      case AppConstants.REOMOVE_ITEM:
+          _removeItem( action.item )
+        break;
+      case AppConstants.INCREASE_ITEM:
+          _increaseItem( action.item )
+        break;
+      case AppConstants.DECREASE_ITEM:
+          _decreaseItem( action.item )
+        break;
+      default:
+
+    }
+    AppStore.emitChange()
+  })
+})
+
+export default AppStore
