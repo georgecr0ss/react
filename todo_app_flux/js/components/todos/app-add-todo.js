abdcd31e6630ef1dbb0fec58f99e58d22ec7a324
Georgi Krustev <georgi.krustev@trader.int>
2016-10-31 18:04:05 +0200

    zxczxc

--- todo_app_flux/js/components/todos/app-add-todo.js
+++ todo_app_flux/js/components/todos/app-add-todo.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import TodoActions from '../../actions/actions'
 export default () => {
+  let input = ""
   return(
     <div>
-      <input />
-      <button>Add todo</button>
+      <input ref = { node => {
+        input = node
+      }} />
+      <button onClick = {()=> {
+        console.log(input.value);
+        // TodoActions.addTodo(input.value)
+        input.value = ""
+      }}>Add todo</button>
     </div>
   )
 }
